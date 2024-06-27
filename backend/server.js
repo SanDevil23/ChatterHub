d8274c725a6a3befdb71bc6ea19be62fb60106cb
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2024-06-27 13:36:54 +0530

    one-to-one message api created

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes")
 
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
@@ -23,6 +24,8 @@ const anonymous = () => {};
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/components/miscellaneous/ProfileModal.js
+++ frontend/src/components/miscellaneous/ProfileModal.js
@@ -1,49 +1,67 @@
-import { ViewIcon  } from '@chakra-ui/icons'
-import { IconButton, ModalFooter, ModalBody, ModalCloseButton, ModalHeader, ModalContent, ModalOverlay, Modal, useDisclosure, Button, Image, Text } from '@chakra-ui/react'
-import React from 'react'
+import { ViewIcon } from "@chakra-ui/icons";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  IconButton,
+  Text,
+  Image,
+} from "@chakra-ui/react";
 
-const ProfileModal = ({user, children}) => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-  return <>
-  {children?(
-    <span onClick={onOpen}>{children}</span>
-    ):(
-    <IconButton display={{base:"flex"}} icon={<ViewIcon/>} onClick={onOpen}/>
-  )}
-  <Modal size="lg" isOpen={isOpen} onClose={onClose} isCentered>
+const ProfileModal = ({ user, children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      {children ? (
+        <span onClick={onOpen}>{children}</span>
+      ) : (
+        <IconButton d={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
+      )}
+      <Modal size="lg" onClose={onClose} isOpen={isOpen} isCentered>
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader fontSize="40px" fontFamily="Work sans" display="flex" justifyContent="center">
+        <ModalContent h="410px">
+          <ModalHeader
+            fontSize="40px"
+            fontFamily="Work sans"
+            d="flex"
+            justifyContent="center"
+          >
             {user.name}
-            </ModalHeader>
+          </ModalHeader>
           <ModalCloseButton />
           <ModalBody
-            display="flex"
+            d="flex"
             flexDir="column"
             alignItems="center"
             justifyContent="space-between"
           >
-            ModalBody
-          </ModalBody>
             <Image
-                borderRadius="full"
-                boxSize="150px"
-                src={user.pic}
-                alt={user.name}
+              borderRadius="full"
+              boxSize="150px"
+              src={user.pic}
+              alt={user.name}
             />
-            <Text fontSize={{base:"29px", md:"30px"}} fontFamily={"Work sans"}>
-                Email:{user.email}
+            <Text
+              fontSize={{ base: "28px", md: "30px" }}
+              fontFamily="Work sans"
+            >
+              Email: {user.email}
             </Text>
+          </ModalBody>
           <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
-            </Button>
-            {/* <Button variant='ghost'>Secondary Action</Button> */}
+            <Button onClick={onClose}>Close</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
-  </>
+    </>
+  );
+};
 
-}
-
-export default ProfileModal
+export default ProfileModal;
